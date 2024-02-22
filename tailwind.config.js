1552dbeffb9996f4ee145537ec95b2be348ee96a
Nazar K. Poladian <65300348+PurpleArcher2@users.noreply.github.com>
2024-02-22 10:05:48 +0200

    1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    fontFamily: {
-      poppins: '"Poppins, sans-serif"',
-    },
     extend: {
       backgroundImage: {
         bgimage: "url('./imgs/background.jpg')",
